6292752861e8757df037f3a54acdc0002ee6e345
Ubuntu <azureuser@ts-ncv3-02.3mcb1mvwvn5ezkxiirlqgexqef.bx.internal.cloudapp.net>
2021-11-23 06:33:54 +0000

    Add train script inside ts_space

---
+++ ts_space/train.sh
@@ -0,0 +1,1 @@
+python train.py --img 640 --batch 24 --epochs 300 --data ts_cotton.yaml --weights '' --cfg yolov5l.yaml --project cotton --name YV5_NV2_017 --model_dir /mnt/ts-cvml-datastore/ts_model/models/cotton
