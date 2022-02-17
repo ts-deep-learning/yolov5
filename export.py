bug fix, previously flipped the wrong dimension and also accepted input size was 640, now input is 1328,1200 which is then resized in the custom layers defined

export.py
@@ -252,7 +252,7 @@ def export_tfjs(keras_model, im, file, prefix=colorstr('TensorFlow.js:')):
 @torch.no_grad()
 def run(data=ROOT / 'data/coco128.yaml',  # 'dataset.yaml path'
         weights=ROOT / 'yolov5s.pt',  # weights path
-        imgsz=(640, 640),  # image (height, width)
+        imgsz=(1200,1328),  # image (height, width)
         batch_size=1,  # batch size
         device='cpu',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
         include=('torchscript', 'onnx', 'coreml'),  # include formats
@@ -288,16 +288,17 @@ def run(data=ROOT / 'data/coco128.yaml',  # 'dataset.yaml path'
     
     print("weights (path to .pt file) is :", weights)
     nc, names = model.nc, model.names  # number of classes, class names
-    #print("nc is {} and names are {}".format(nc,names))
+    print("nc is {} and names are {}".format(nc,names))
     #print("Model")
     #print(model)
     ########### END OF MODEL CHANGES ##########
     if RUN_EXPORT:
         # Input
-        gs = int(max(model.stride))  # grid size (max stride)
-        imgsz = [check_img_size(x, gs) for x in imgsz]  # verify img_size are gs-multiples
+        #gs = int(max(model.stride))  # grid size (max stride)
+        #imgsz = [check_img_size(x, gs) for x in imgsz]  # verify img_size are gs-multiples
         im = torch.zeros(batch_size, 3, *imgsz).to(device)  # image size(1,3,320,192) BCHW iDetection
 
+        print("dictated input image size is", imgsz)
         # Update model
         if half:
             im, model = im.half(), model.half()  # to FP16

models/experimental.py
@@ -94,14 +94,15 @@ class Custom_Layer(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self,input):
-        # flip channels to go from bgr to rgb
-        x = torch.flip(input,[2])
+    def forward(self,input_tensor):
+        # flip channels to go from bgr to rgb, the input tensor automatically gets dimension added
+        # input tensor becomes [1,3,1200,1328] and we will flip the 2nd dimension
+        flipped_image = torch.flip(input_tensor,[1])
         interpolation = T.InterpolationMode.NEAREST
-        transformed_img = torch.nn.Sequential(T.Resize((640,640),interpolation=interpolation))
-        x = transformed_img(x)
-        #x = torch.unsqueeze(x,0)
-        return x
+        transformer = torch.nn.Sequential(T.Pad((0,64)),T.Resize((640,640),interpolation=interpolation))
+        transformed_tensor = transformer(flipped_image)
+        #transformed_tensor = torch.unsqueeze(transformed_tensor,0)
+        return transformed_tensor
 
 class Custom_Model(nn.Module):
     def __init__(self, pretrained_model, nc, names, stride):

models/test.py
@@ -13,7 +13,10 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 import torchvision.transforms as T
 from torchvision.io import read_image
+from torchvision.utils import save_image
 # from torchvision import datasets, transforms
+from PIL import Image
+import matplotlib.pyplot as plt
 
 RUN_MODEL = False
 
@@ -37,11 +40,11 @@ def placeholder():
     #make_dot(dummy_model, params=dict(model.named_parameters()), show_attrs=True, show_saved=True)
 
 def transformer(inp_tensor):
-    transformed_img = torch.flip(inp_tensor,[2])
+    flipped_image = torch.flip(inp_tensor,[0])
     interpolation = T.InterpolationMode.NEAREST
-    transformed_img = torch.nn.Sequential(T.Resize((640,640),interpolation=interpolation))
-    transformed_tensor = transformed_img(inp_tensor)
-    transformed_tensor = torch.unsqueeze(transformed_tensor,0)
+    transformer = torch.nn.Sequential(T.Pad((0,64)),T.Resize((640,640),interpolation=interpolation))
+    transformed_tensor = transformer(flipped_image)
+    #transformed_tensor = torch.unsqueeze(transformed_tensor,0)
     return transformed_tensor
 
 def load_yaml(cfg='yolov5m.yaml'):
@@ -88,9 +91,16 @@ if __name__ == '__main__':
     #print(ch)
     
     inp_img = read_image("/home/sush/depth_0.jpg")
-    print(inp_img.size())
+
+    print("input image size",inp_img.size())
     transformed_tensor = transformer(inp_img)
-    print(transformed_tensor.size())
+    print("transformed image size",transformed_tensor.size())
+    #transformed_tensor = transformed_tensor.cpu().detach().numpy()
+    #im = Image.fromarray(transformed_tensor)
+    #im.save("/home/sush/TS/depth_img_transformed.png")
+    plt.imshow(transformed_tensor.permute(1, 2, 0))
+    plt.show()
+    #save_image(transformed_tensor,'/home/sush/depth_img_transformed.png')
     '''
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     print(f'Using {device} device')