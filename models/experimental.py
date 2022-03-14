bb39787b83168300cfd7f014b867ece45f15e22b
sushanthj <sushanth.jayanth@gmail.com>
2022-03-14 10:23:43 +0530

    trial with padding for onnx-gs -modified .permute

--- models/experimental.py
+++ models/experimental.py
@@ -136,7 +136,8 @@ class Custom_Layer(nn.Module):
         interpolation = T.InterpolationMode.NEAREST
         transformer = torch.nn.Sequential(T.Pad((0,64)),T.Resize((640,640),interpolation=interpolation))
         transformed_tensor = transformer(flipped_image)
-        transformed_tensor_2 = transformed_tensor.permute(0, 3, 1, 2)
+        print(transformed_tensor.size())
+        transformed_tensor_2 = transformed_tensor.permute(2, 0, 1)
         return transformed_tensor_2
 
 class Custom_Model(nn.Module):
